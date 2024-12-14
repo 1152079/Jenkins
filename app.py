31fc7159c9a7a45b84da7eedb4a2b6d34225af0a
1152079 <aehcf01@inbox.ru>
2024-12-14 15:55:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def funny():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False, user_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
